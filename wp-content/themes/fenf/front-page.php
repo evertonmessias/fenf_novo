7482b441ff516516128e19da5902b4ce5e6888db
evertonmessias <everton.messias@gmail.com>
2022-01-26 18:36:43 -0300

    26-01-2022_18:36:43

--- wp-content/themes/fenf/front-page.php
+++ wp-content/themes/fenf/front-page.php
@@ -321,7 +321,7 @@
 		<div class="container" data-aos="fade-up">
 
 			<div class="section-title">
-				<h2>Testimonials</h2>
+				<h2>Depoimentos</h2>
 				<p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
 			</div>
 
@@ -493,7 +493,7 @@
 		<div class="container" data-aos="fade-up">
 
 			<div class="section-title">
-				<h2>Gallery</h2>
+				<h2>Galeria de Imagens</h2>
 				<p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
 			</div>
 
